791f7d92b9924c32138177fae346c5a4e5c63580
Saravanan Ganesan <saravanan2991@gmail.com>
2025-04-07 02:10:24 +0530

    Add agriculture-agent-system/Validators/CropDataValidator.cs 1743972022855

--- agriculture-agent-system/Validators/CropDataValidator.cs
+++ agriculture-agent-system/Validators/CropDataValidator.cs
@@ -7,25 +7,24 @@ namespace AgricultureAgentSystem.Validators
     {
         public CropDataValidator()
         {
-            RuleFor(x => x.Region)
-                .NotEmpty().WithMessage("Region is required")
-                .Length(2, 100).WithMessage("Region must be between 2 and 100 characters");
-
-            RuleFor(x => x.CropName)
+            RuleFor(x => x.Name)
                 .NotEmpty().WithMessage("Crop name is required")
-                .Length(2, 50).WithMessage("Crop name must be between 2 and 50 characters");
+                .Length(2, 50).WithMessage("Crop name must be between 2-50 characters");
 
-            RuleFor(x => x.SoilType)
-                .NotEmpty().WithMessage("Soil type is required");
+            RuleFor(x => x.Season)
+                .NotEmpty().WithMessage("Growing season is required")
+                .Length(2, 20).WithMessage("Season must be between 2-20 characters");
 
-            RuleFor(x => x.OptimalSeason)
-                .NotEmpty().WithMessage("Optimal season is required");
+            RuleFor(x => x.GrowthDays)
+                .GreaterThan(0).WithMessage("Growth days must be positive")
+                .LessThanOrEqualTo(365).WithMessage("Growth period cannot exceed 1 year");
 
             RuleFor(x => x.WaterRequirement)
-                .GreaterThan(0).WithMessage("Water requirement must be greater than 0");
+                .GreaterThan(0).WithMessage("Water requirement must be positive");
 
-            RuleFor(x => x.YieldPotential)
-                .GreaterThan(0).WithMessage("Yield potential must be greater than 0");
+            RuleFor(x => x.SoilType)
+                .NotEmpty().WithMessage("Soil type is required")
+                .Length(2, 30).WithMessage("Soil type must be between 2-30 characters");
         }
     }
 }
