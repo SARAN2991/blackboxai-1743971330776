3753809726414715070789beefda2a5d5fc411b3
Saravanan Ganesan <saravanan2991@gmail.com>
2025-04-08 15:16:02 +0530

    Add agri-hub/src/app/core/navigation/navigation.component.ts 1744105559344

---
+++ agri-hub/src/app/core/navigation/navigation.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-navigation',
+  templateUrl: './navigation.component.html',
+  styleUrls: ['./navigation.component.scss']
+})
+export class NavigationComponent {
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) {}
+
+  logout() {
+    this.authService.logout();
+    this.router.navigate(['/login']);
+  }
+}
